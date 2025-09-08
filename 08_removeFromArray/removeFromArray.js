ba711cb9d9f2a1c9f0bd5e3a2a4ab93586a4240b
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-07 22:44:45 -0500

    Completed 08_removeFromArray assignment

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+    let array = args[0]
+    let removeArr = args.slice(1)
+    return array.filter((item) => !removeArr.includes(item));
+
 };
 
 // Do not edit below this line
