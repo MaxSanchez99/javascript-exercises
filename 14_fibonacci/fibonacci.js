9ec849e024c695e33f0490482b22634e1f9f0f4c
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-14 16:08:14 -0500

    Completed javascript exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num == 'string') {
+        num = +num;
+    }
 
+    if (num < 0) return "OOPS"
+
+    if (num == 0) return 0;
+
+    let prev = 1;
+    let total = 1;
+
+    for (let i = 1; i < num; i++) {
+        let temp = total;
+        total += prev;
+        prev = temp;
+    }
+
+    return prev;
 };
 
 // Do not edit below this line
