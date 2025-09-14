f1d8791f55f8dbebd52d52632fa59b21c0626100
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-14 15:50:02 -0500

    Completed javascript exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let noSpaces = str.replaceAll(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+    let i = 0
+    let j = noSpaces.length - 1;
+
+    while (i < j) {
+        if (noSpaces[i] != noSpaces[j]) return false;
+        i++;
+        j--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
