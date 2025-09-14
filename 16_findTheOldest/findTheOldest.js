352311705cad7a09820c34b2a26d49cbe9cce1b3
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-14 16:28:04 -0500

    Completed javascript exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
 
+        let first = 0;
+        let second = 0;
+
+        if (!('yearOfDeath' in oldest)) {
+            first = currentYear - oldest.yearOfBirth;
+        }
+        else {
+            first = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        if (!('yearOfDeath' in person)) {
+            second = currentYear - person.yearOfBirth;
+        }
+        else {
+            second = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (first > second) {
+            return oldest;
+        }
+        else {
+            return person;
+        }
+    })
 };
 
 // Do not edit below this line
